c48222faea2a128dd2512a5c607141675413fd6a
Evan Lin <evan.lin.3.141@gmail.com>
2026-09-15 14:53:02 +0000

    FEAT: Skeleton DTO added for all classes

--- app/src/main/java/com/neueda/leap/Main.java
+++ app/src/main/java/com/neueda/leap/Main.java
@@ -1,4 +1,3 @@
-package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) 
---
+++ app/src/main/java/com/neueda/leap/dto/AccountResponseDTO.java
@@ -0,0 +1,7 @@
+package com.neueda.leap.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountResponseDTO {
+}
