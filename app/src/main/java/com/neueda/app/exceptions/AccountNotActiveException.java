47f993236f4ab49e759fd2d262c8fc9e69f6d7e8
Jes Mariya <jesmariyaarun@gmail.com>
2026-09-16 14:52:05 +0000

    refactor: remove unnecessary methods from AccountNotActiveException

--- app/src/main/java/com/neueda/app/exceptions/AccountNotActiveException.java
+++ app/src/main/java/com/neueda/app/exceptions/AccountNotActiveException.java
@@ -1,54 +1,17 @@
 package com.neueda.app.exceptions;
 
-import com.neueda.app.enums.AccountStatus;
-
 /**
- * Exception thrown when an operation is attempted on an account that is not active.
- * This includes operations on suspended, inactive, or closed accounts.
+ * Exception thrown when an account is suspended or closed.
  */
 public class AccountNotActiveException extends TradingException {
 
-    private final Long accountId;
-    private final AccountStatus accountStatus;
-
-    /**
-     * Constructs a new AccountNotActiveException for the specified account.
-     *
-     * @param accountId     the ID of the account
-     * @param accountStatus the current status of the account
-     */
-    public AccountNotActiveException(Long accountId, AccountStatus accountStatus) {
-        super(String.format("Account %d is not active. Current status: %s", accountId, accountStatus));
-        this.accountId = accountId;
-        this.accountStatus = accountStatus;
-    }
-
-    /**
-     * Constructs a new AccountNotActiveException with a custom detail message.
-     *
-     * @param message the detail message
-     */
     public AccountNotActiveException(String message) {
         super(message);
-        this.accountId = null;
-        this.accountStatus = null;
     }
 
-    /**
-     * Gets the account ID.
-     *
-     * @return the account ID
-     */
-    public Long getAccountId() {
-        return accountId;
+    public AccountNotActiveException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    /**
-     * Gets the account status.
-     *
-     * @return the account status
-     */
-    public AccountStatus getAccountStatus() {
-        return accountStatus;
-    }
+  
 }
