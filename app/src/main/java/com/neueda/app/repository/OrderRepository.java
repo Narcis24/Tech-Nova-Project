30d0118b8b608da7cfe509e8e3312483d8804696
Narcis <narcisbalint1122@gmail.com>
2026-09-23 14:43:00 +0000

    Added method to retrieve the Order history of an account

--- app/src/main/java/com/neueda/app/repository/OrderRepository.java
+++ app/src/main/java/com/neueda/app/repository/OrderRepository.java
@@ -4,7 +4,9 @@ import com.neueda.app.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findByAccountId(String accountId);
 }
