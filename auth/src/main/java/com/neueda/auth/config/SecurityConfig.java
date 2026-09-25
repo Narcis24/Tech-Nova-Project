6abe3e62b7d48ddae97ff216905c677f360bfe1e
Evan Lin <evan.lin.3.141@gmail.com>
2026-09-25 10:45:11 +0000

    FEAT: Added JwtAuthenticationHandler
    Custom JwtAuthenticationHandler added to show the ErrorResponse DTO, as it get's rejected before it hits the GlobalExceptionHandler

--- auth/src/main/java/com/neueda/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/neueda/auth/config/SecurityConfig.java
@@ -7,19 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.neueda.auth.filter.JwtAuthenticationFilter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-        
-    private final JwtAuthenticationFilter jwtAuthenticationFilter; 
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -33,10 +23,8 @@ public class SecurityConfig {
                 .requestMatchers("/v1/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui.html").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()  // Everything else is also public
             )
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .formLogin().disable()
             .httpBasic().disable()
             .csrf().disable();
